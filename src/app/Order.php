ef7dbced2bf2a74f1c6712d441803a42f78e74f6
Florin Mirosnicencu <florin.mirosnicencu@sykescottages.co.uk>
2020-01-01 16:30:45 +0000

    -Green- Now orders can be cancelled and the tickets released.

--- src/app/Order.php
+++ src/app/Order.php
@@ -14,4 +14,13 @@ class Order extends Model
     {
         return $this->hasMany(Ticket::class);
     }
+
+    public function cancel()
+    {
+        foreach ($this->tickets as $ticket) {
+            $ticket->update(['order_id' => null]);
+        }
+        $this->delete();
+    }
+
 }
---
+++ src/tests/unit/Billing/OrderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+use App\Order;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class OrderTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * @test
+     */
+    public function test_the_tickets_are_released_when_the_order_is_canceled()
+    {
+        $concert = factory(\App\Concert::class)->create();
+        $concert->addTickets(10);
+        $order = $concert->orderTickets('jane@example.com', 5);
+        $this->assertEquals(5, $concert->ticketsRemaining());
+
+        $order->cancel();
+        $this->assertEquals(10, $concert->ticketsRemaining());
+        $this->assertNull(Order::find($order->id));
+
+    }
+
+}
