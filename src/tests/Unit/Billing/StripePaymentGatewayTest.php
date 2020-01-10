11e00f80fd31414a4e87cc0d81cb93af0376d1ae
Florin Mirosnicencu <florin.mirosnicencu@sykescottages.co.uk>
2020-01-10 14:39:24 +0000

    -Green- Add the ability to not run the integration tests.

--- src/tests/Unit/Billing/StripePaymentGatewayTest.php
+++ src/tests/Unit/Billing/StripePaymentGatewayTest.php
@@ -7,12 +7,28 @@ namespace Tests\Unit\Billing;
 use App\Billing\StripePaymentGateway;
 use Stripe\Charge;
 use Stripe\Exception\ApiErrorException;
+use Stripe\Stripe;
 use Stripe\Token;
 use Tests\TestCase;
 
+/**
+ * Class StripePaymentGatewayTest
+ * @package Tests\Unit\Billing
+ * @group integration
+ */
 class StripePaymentGatewayTest extends TestCase
 {
     const TEST_STRIPE_API_KEY = 'sk_test_a2Af9Av8MK9CkPpmpEhi48IJ';
+    /**
+     * @var Charge
+     */
+    private $lastCharge;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->lastCharge = $this->lastCharge();
+    }
 
     /** @test
      * @throws ApiErrorException
@@ -20,11 +36,10 @@ class StripePaymentGatewayTest extends TestCase
     public function charges_with_a_valid_payment_token_are_successful()
     {
         $paymentGateway = new StripePaymentGateway(self::TEST_STRIPE_API_KEY);
-        $lastCharge = $this->lastCharge();
 
         $paymentGateway->charge(2500, $this->validToken());
 
-        $this->assertCount(1, $this->newCharges($lastCharge));
+        $this->assertCount(1, $this->newCharges());
         $this->assertEquals(2500, $this->lastCharge()->amount);
     }
 
@@ -34,6 +49,8 @@ class StripePaymentGatewayTest extends TestCase
      */
     private function lastCharge(): Charge
     {
+        Stripe::setApiKey(self::TEST_STRIPE_API_KEY);
+
         return Charge::all([
             'limit' => 1,
         ])['data'][0];
@@ -55,11 +72,11 @@ class StripePaymentGatewayTest extends TestCase
         ])->id;
     }
 
-    private function newCharges(Charge $endingBefore): array
+    private function newCharges(): array
     {
         return Charge::all([
             'limit' => 1,
-            'ending_before' => $endingBefore->id
+            'ending_before' => $this->lastCharge->id
         ])['data'];
     }
 }
