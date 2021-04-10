e0c1b1e41814e69789ee8417757c2152abb09a16
Florin Mirosnicencu <florinmirosnicencu@gmail.com>
2021-04-10 20:30:54 +0100

    -GREEN- Docker-compose cleanup

--- docker/php/config/xdebug.ini.tpl
+++
@@ -1,4 +0,0 @@
-xdebug.remote_enable=1
-xdebug.remote_port=9001
-xdebug.remote_handler=dbgp
-xdebug.remote_autostart=0
--- docker/php/scripts/xdebug.sh
+++
@@ -1,7 +0,0 @@
-if [ ! -f docker/php/config/xdebug.ini ]; then
-    read -p "enter Xdebug ide key:[docker]: " IDEKEY
-    IDEKEY=${IDEKEY:-docker}
-    cp ../config/xdebug.ini.tpl docker/php/config/xdebug.ini
-    printf "\nxdebug.remote_host=$DOCKER_HOST_IP" >> docker/php/config/xdebug.ini
-    printf "\nxdebug.idekey=$IDEKEY" >> docker/php/config/xdebug.ini
-fi
